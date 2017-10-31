5b8730af7b355af516d225e479986d5f95b34cd8
pouiscakes <louisrlin@gmail.com>
2017-10-31 16:54:58 -0700

    moved form into separate php file to make it easier to control

--- course_equivalence.php
+++ course_equivalence.php
@@ -4,16 +4,12 @@ $conn = new mysqli($servername, $username, $password, $db_name);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 session_start(); // must start session before any HTML
-
 $user_check = $_SESSION['login_user'];
-
 $ses_sql = mysqli_query($conn,"select username from users where username = '$user_check' ");
-
 $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
-
 $login_session = $row['username'];
-
 if(!isset($_SESSION['login_user'])){
   header("location:login.php");
 }
@@ -86,48 +82,12 @@ if (!empty($outside_school) && !empty($outside_course) &&
   <div id="live_data"></div> 
 
 <?php
-
-// $output = '';
-// // sql to select data
-// $sql = "SELECT id, outside_school, outside_course, scu_course, equivalence, notes FROM courses";
-// $result = $conn->query($sql);
-
-// $output .= "
-//     <table id='course_list'>
-//         <tr class='table_header'>
-//           <th>Outside School</th>
-//           <th>Outside Course</th>
-//           <th>SCU Course</th>
-//           <th>Equivalent</th>
-//           <th>Notes</th>
-//           <th>Delete</th>
-//         </tr>";
-// if ($result->num_rows > 0) {
-	
-//     // output data of each row in table
-//     while($row = $result->fetch_assoc()) {
-//     	$output .= '
-//     		<tr>
-//     			<td class="outside_school" data-id1="'.$row["id"].'" contenteditable>' . $row["outside_school"] . '</td>
-//     			<td class="outside_course" data-id2="'.$row["id"].'" contenteditable>' . $row["outside_course"] . '</td>
-//     			<td class="scu_course" data-id3="'.$row["id"].'" contenteditable>' . $row["scu_course"] . '</td>
-//     			<td class="equivalence" data-id4="'.$row["id"].'" contenteditable>' . $row["equivalence"] . '</td>
-//     			<td class="notes" data-id5="'.$row["id"].'" contenteditable>' . $row["notes"] . '</td>
-//           <td><button type="button" name="delete_btn" data-id6="'.$row["id"].'" class="btn btn-xs btn-danger btn_delete">x</button></td>
-//     		</tr>'; 
-//     }
-//     $output .= "</table>";
-// } else {
-//     $output .= "0 results";
-// }
-// echo $output;
-
 mysqli_close($conn);
-    
 ?>
 
   </body>
 </html>
+
 
 <script>  
 // JavaScript and jQuery for editing data in tables
