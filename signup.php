7bace0a2b75040f70d52d71a1c35318f74a7d3cf
pouiscakes <louisrlin@gmail.com>
2017-11-01 22:24:29 -0700

    fix css filepath

--- signup.php
+++ signup.php
@@ -10,8 +10,8 @@ session_start(); // must start session before any HTML
     <title>Signup</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
-    <link href="../css/bootstrap.css" rel="stylesheet" media="screen">
-    <link href="../css/main.css" rel="stylesheet" media="screen">
+    <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="css/main.css" rel="stylesheet" media="screen">
   </head>
 
   <body>
--- signup1.php
+++ signup1.php
@@ -46,8 +46,8 @@
     <title>Course Equivalence</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
-    <link href="../css/bootstrap.css" rel="stylesheet" media="screen">
-    <link href="../css/main.css" rel="stylesheet" media="screen">
+    <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="css/main.css" rel="stylesheet" media="screen">
   </head>
 
   <body>
